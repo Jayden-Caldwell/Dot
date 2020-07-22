c53003de9b1f753835e0ec8323cd17350764ec34
mcdonhj1 <mcdonhj1@D207-61591>
2020-07-22 14:11:55 +1200

    have added radio buttons for yes no answers that append to a list when next button is pressed

--- surveyProgramTkinterTest/surveyProgramTkinterTest.py
+++ surveyProgramTkinterTest/surveyProgramTkinterTest.py
@@ -17,35 +17,40 @@ class Survey(tk.Frame):
     def __init__(self, file, root, canvas):
         self.file = file
         self.questions = []
+        self.answers = []
         self.readIn()
         self.root = root
         self.canvas = canvas
         self.count = 0
+        self.current_answer = StringVar()
+        self.easy_frame = Frame(self.canvas,bg="white")
+        self.ques = Label(self.easy_frame,text =self.questions["question"][self.count],font="calibri 12",bg="white")
+        self.nextQuestion = Button(self.easy_frame,command=self.display,text="Next")
+        self.yes_choice = Radiobutton(self.easy_frame,text="yes",font="calibri 10",value="yes",variable = self.current_answer,bg="white")
+        self.no_choice = Radiobutton(self.easy_frame,text="No",font="calibri 10",value="no",variable = self.current_answer,bg="white")
+        
         
     def readIn(self):
         self.questions = pd.read_csv(self.file)         #reads in the questions file
 
+
     def ask_questions(self):#asks questions
         
-    
-    
-        easy_frame = Frame(self.canvas,bg="white")
-        easy_frame.place(relwidth=0.8,relheight=0.8,relx=0.1,rely=0.1) 
-        global ques
-        ques = Label(easy_frame,text =self.questions["question"][self.count],font="calibri 12",bg="white")
-        ques.place(relx=0.5,rely=0.2,anchor=CENTER)           
-        answers = []
-        global nextQuestion
-        nextQuestion = Button(easy_frame,command=self.display,text="Next")
-        nextQuestion.place(relx=0.87,rely=0.82,anchor=CENTER)
+        self.easy_frame.place(relwidth=0.8,relheight=0.8,relx=0.1,rely=0.1) 
+        
+        self.ques.place(relx=0.5,rely=0.2,anchor=CENTER)           
+        
+        self.nextQuestion.place(relx=0.87,rely=0.82,anchor=CENTER)
+        self.yes_choice.place(relx=0.5,rely=0.42,anchor=CENTER)
+        self.no_choice.place(relx=0.5,rely=0.52,anchor=CENTER)
         
         self.root.mainloop()
         
         
         
-        for index, row in self.questions.iterrows():    #goes through asking each question
+    """for index, row in self.questions.iterrows():    goes through asking each question
             
-            '''text.insert(tk.INSERT, row['question'] + ":")  
+           text.insert(tk.INSERT, row['question'] + ":")  
             text.pack()     
             ans = input('Type Y(yes) or N(n): ')    #asks for input
             if ans[0].upper() == 'Y':               #checks first letter of input and capitilizes
@@ -56,10 +61,12 @@ class Survey(tk.Frame):
                 break
             else:
                 print('Please type either \'Y\' or \'N\'') #asks user to type y or n(temp)
-        self.write_answers_to_file(answers) '''            #send answers list to be written to answers csv
+        self.write_answers_to_file(answers) """           #send answers list to be written to answers csv
 
     def display(self):
         
+        self.answers.append(self.current_answer.get())
+        print(self.answers)
         number_of_questions = len(self.questions)
         self.count = self.count + 1
         if self.count == number_of_questions:
@@ -67,13 +74,15 @@ class Survey(tk.Frame):
                 #e.destroy()
                 #showMark(score)
         elif self.count == (number_of_questions-1):
-            nextQuestion.configure(text='finish',command=display)
+            self.nextQuestion.configure(text='finish',command=self.finish)
             
         if self.count <= number_of_questions:
-            ques.configure(text =self.questions["question"][self.count])
+            self.ques.configure(text =self.questions["question"][self.count])
             
-            
-            
+    
+    def finish(self):
+        
+                   
             
     
     def write_answers_to_file(self, answers):           #writes answers to csv
